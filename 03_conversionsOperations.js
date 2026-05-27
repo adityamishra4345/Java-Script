a383bd5c266ef5194fdf110e63c9efe4dee4e6c2
adityamishra4345 <adityamishra4345@gmail.com>
2026-05-27 20:31:12 +0530

    Operators in JS

--- 03_conversionsOperations.js
+++ 03_conversionsOperations.js
@@ -23,3 +23,33 @@ console.log(bool)
 //"" => false
 //"adi" => true
 //similar conversion for String etc
+
+//operations like 2+3 etc
+let value=3
+let negValue=-value
+console.log(negValue)
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2^4)//Xor operator
+console.log(2>4)//shift operator
+console.log(2**4)//power
+
+let str1="Hello"
+let str2="Aditya"
+let str3=str1+str2//concatenation without spaces
+console.log(str3)
+console.log("2"+3)
+console.log("2"+2+3)//since pehle 2 str hai so rest bhi str me ho jaigi
+console.log(2+2+"3")//pehle int hai toh jaha tk possible hai tbb tk int and rest str
+console.log(2+2+"3"+2)//pehle int hai toh jaha tk possible hai tbb tk int and rest str ke baad ke sab str
+console.log(2+3*5%3)
+//but try to avoid this type of things and use brackets although bodmas for programming is follwed 
+let n1 , n2, n3;//multiple declaration
+ n1=101;
+ n2=101
+ console.log(--n1)//first decrement then print   100
+ console.log(n2--)//first print then decrement   101
+ //== checks like "2"==2 =>true  as convers str 2 to num 
+ //===cheks everything along with datatype so "2"===2  =>false
+ 
