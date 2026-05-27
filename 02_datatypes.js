c5afefd9dbe34e8515427ba45098811b7cd71979
adityamishra4345 <adityamishra4345@gmail.com>
2026-05-27 17:57:12 +0530

    DataTypes

---
+++ 02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";/*treates all JS code as newer version 
+its done by js community to save the older version but in current 
+days almost all codes follow strict mode */
+
+
+//alert("Hello")-> can be directly use in browser inspect but here differnt syntax
+
+
+
+let name="Adi"//string data type
+let age=18    //integer 
+let login=false  //boolean=> true/false
+
+let a=null;
+//null =>standalone value which shows empty i.e we can assingn a var to be null
+
+let b;
+//undefined  => value is not defined
+
+//symbol => shows uniqueness
+
+//object
+
+console.log(typeof null); //gives output is object
+console.log(typeof undefined) //gives output undefined
+
+//typeof is used to find from which data type its belongs to
