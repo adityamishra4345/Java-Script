6124981552206dc77d6fb2709a0c0219760afef9
adityamishra4345 <adityamishra4345@gmail.com>
2026-05-28 09:28:53 +0530

    numbers done

---
+++ 05_numbers&Maths.js
@@ -0,0 +1,9 @@
+const score=40.8623
+let scr =100000000
+const balance=new Number(100)//Another method to declare number i.e writes explicitly number
+console.log(balance)
+console.log(balance.toString())//changing to str will allows to use all methods of string
+console.log(balance.toString().length)
+console.log(balance.toFixed(3))//fixed value after decimal
+console.log(score.toPrecision(3))//precision and also rounds off 
+console.log(scr.toLocaleString())//standard comma separation
