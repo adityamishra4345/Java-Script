1a6d1922921a0c39a9d3ab7a39c3650f7d8f8e43
adityamishra4345 <adityamishra4345@gmail.com>
2026-05-28 09:39:52 +0530

    maths done

--- 05_numbers&Maths.js
+++ 05_numbers&Maths.js
@@ -1,4 +1,4 @@
-const score=40.8623
+/*const score=40.8623
 let scr =100000000
 const balance=new Number(100)//Another method to declare number i.e writes explicitly number
 console.log(balance)
@@ -7,3 +7,15 @@ console.log(balance.toString().length)
 console.log(balance.toFixed(3))//fixed value after decimal
 console.log(score.toPrecision(3))//precision and also rounds off 
 console.log(scr.toLocaleString())//standard comma separation
+*/
+
+console.log(Math.abs(-3))
+console.log(Math.round(100.98))
+console.log(Math.max(3,5,4,9))
+console.log(Math.random())//random value between 0 & 1
+console.log((Math.random()*10)+1)
+console.log(Math.floor((Math.random()*100)+1))//random values greter than 1
+const max=20
+const min=10
+console.log(Math.floor(Math.random() * (max - min + 1 )) + min)//random value between 10 & 20
+console.log(Math.random())//random value between 0 & 1
