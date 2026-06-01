b507cd2aaf2de11994f3ec2e2f3b1ffe88a4d606
adityamishra4345 <adityamishra4345@gmail.com>
2026-06-01 10:24:44 +0530

    function updated

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -42,3 +42,41 @@ console.log(sales({
     price:1000
 }))
 //similarly we can do for arrays
+
+//syntax 2 for fn declaration
+const a=function(num){
+    return num+2
+}
+console.log(a(5))
+
+
+//in normal function declaration we can call function above its declaration but here since
+//we are storing in a variable so we cant do so
+
+//this  is user to refer current context for an object but not in a fun
+const mnn ={ 
+    username:"Aditya",
+    life:"going good",
+    welcome : function(){
+        console.log(` Hii ${this.username}, welcome onboard`)
+    }
+}
+mnn.welcome()
+mnn.username="animesh"
+mnn.welcome()
+
+// another definition for functions
+const chai = () => {
+    let username = "Aditya"
+    console.log(`${username} is the king`)
+}
+chai()
+
+const addtwo=(n1,n2) => {
+    return n1+n2
+}
+//implicit return
+const addn = (n1,n2) => n1+n2//returns n1 + n2 without return keyword 
+const addnn=(n1,n2)=>(n1+n2)//returns n1 + n2 without return keyword better if u use
+//Using the parentheses ( ) here is completely valid, but for a simple math equation, it is optional. However,
+//  using parentheses becomes mandatory in one specific scenario: Returning an Object.
